46238e2e8e892ea4265fbb73030c50626410a254
dan <875126243@qq.com>
2024-06-11 16:04:22 +0800

    fix update app .jsx

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <h2>Home</h2>
+            <ul> 
+                <li>
+                    <NavLink to={"/home"}> 钱包登录 </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/home"}> 签名操作 </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/home"}> 发送交易 </NavLink>
+                </li>
+            </ul>
+        </div>
     )
 }
 
